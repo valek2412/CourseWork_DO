0d5df290a1e77c396d383f05b42f0c46d82eef6e
Valek2412 <valoleg99@gmail.com>
2020-05-03 20:09:07 +0300

    Initial React project

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { hot } from 'react-hot-loader/root';
+
+
+const App = () => (
+  // eslint-disable-next-line react/jsx-filename-extension
+  <div className="App">
+    
+  </div>
+);
+
+export default hot(App);
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// eslint-disable-next-line import/extensions
+import App from './App.js';
+
+// eslint-disable-next-line react/jsx-filename-extension
+ReactDOM.render(<App />, document.getElementById('root'));
